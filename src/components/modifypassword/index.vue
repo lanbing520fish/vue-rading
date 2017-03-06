edf6d3022672000513d64ed2b89f27ce21dbe5a8
mac <mac@MacBook-Pro.local>
2017-03-06 17:08:35 +0800

    修改绑定手机＋密码修改

--- src/components/modifypassword/index.vue
+++ src/components/modifypassword/index.vue
@@ -1,12 +1,129 @@
 <template>
-  <div>
-    modifypassword
-  </div>
+	<div class="right-result">
+		<div class="right-title">
+			<span class="iconfont">&#xe7ad;</span>修改密码：
+		</div>
+		<div class="fn-left password-width">	
+			<el-form ref="form" :model="form" class="mt75">
+				<el-form-item>
+					<el-col :span="6" class="text-right">旧密码：</el-col>
+					<el-col :span="18">
+						<el-input v-model="form.phonenumber"></el-input>
+					</el-col>
+				</el-form-item>
+				<el-form-item>
+					<el-col :span="6" class="text-right">新密码：</el-col>
+					<el-col :span="18">
+						<el-input v-model="form.newphonenumber"></el-input>
+					</el-col>
+				</el-form-item>
+				<el-form-item>
+					<el-col :span="6" class="text-right">确认密码：</el-col>
+					<el-col :span="18">
+						<el-input v-model="form.newphonenumber"></el-input>
+					</el-col>
+				</el-form-item>
+				<el-form-item>
+					<el-col :span="6" class="text-right">验证码：</el-col>
+					<el-col :span="12">
+						<el-input placeholder="请输入验证码" v-model="form.testgetcode"></el-input>
+					</el-col>
+					<el-col :span="6" class="text-right">
+						<el-button class="refresh" type="primary" @click="onSubmit">
+							<span class="iconfont"><img src="./refresh.png" alt=""></span>
+							<span class="clickrefresh">点击刷新</span>
+						</el-button>
+					</el-col>
+				</el-form-item>
+				<el-form-item>
+					<el-col :offset="6">
+						<el-button class="present" type="primary" @click="onSubmit">提&nbsp;&nbsp;&nbsp;&nbsp;交</el-button>
+					</el-col>
+				</el-form-item>
+	  		</el-form>
+  		</div>
+  		<div class="fn-right rule-width">
+  			<img src="./passwordrule.jpg" alt="">
+  		</div>
+	</div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {}
+  export default {
+    data() {
+      return {
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        }
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!');
+      }
+    }
+  }
 </script>
 
 <style lang="less" rel="stylesheet/less">
+	.mt75{
+		margin-top: 75px;
+	}
+	.text-right{
+		text-align: right;
+	}
+	.right-result .right-title .iconfont{
+		margin-left: 15px;
+		margin-right: 12px;
+		font-size: 17px;
+		vertical-align: middle;
+	}
+	.right-result{
+		padding: 0px 3px;
+	}
+	.right-title{
+		border-bottom: 1px solid #c3d2d5;
+		height: 40px;
+		line-height: 40px;
+		font-weight: bold;
+	}
+	.present{
+		background: #58bde4;
+		border-color: #58bde4;
+	}
+	.password-width{
+		width: 480px;
+	}
+	.rule-width{
+		width: 335px;
+		margin-right: 80px;
+		background: #ccc;
+		margin-top: 34px;
+		height: 34px;
+	}
+	.refresh{
+		background: #f5f5f5;
+		border-color: #c3c9d0;
+		color: #323232;
+		padding: 6px 10px;
+	}
+	.refresh .iconfont{
+		float: left;
+		width: 21px;
+		height: 21px;
+		margin-right: 6px;
+		padding: 0px;
+	}
+	.refresh .clickrefresh{
+		float: left;
+		line-height: 20px;
+		vertical-align: middle;
+	}
 </style>
--- src/components/modifyphone/index.vue
+++ src/components/modifyphone/index.vue
@@ -1,12 +1,97 @@
 <template>
-  <div>
-    modifyphone
-  </div>
+	<div class="right-result">
+		<div class="right-title">
+			<span class="iconfont">&#xe7ad;</span>修改绑定手机：
+		</div>
+		<el-form ref="form" :model="form" class="mt75">
+			<el-form-item>
+				<el-col :span="6" class="text-right">原手机号码：</el-col>
+				<el-col :span="10">
+					<el-input placeholder="13951990932" v-model="form.phonenumber" :disabled="true"></el-input>
+				</el-col>
+			</el-form-item>
+			<el-form-item>
+				<el-col :span="6" class="text-right">新手机号码：</el-col>
+				<el-col :span="10">
+					<el-input v-model="form.newphonenumber"></el-input>
+				</el-col>
+			</el-form-item>
+			<el-form-item>
+				<el-col :span="6" class="text-right">手机验证码：</el-col>
+				<el-col :span="10" class="getcode">
+					<el-input v-model="form.testgetcode"></el-input>
+					<el-button class="testgetcode" type="primary" @click="onSubmit">获取验证码</el-button>
+				</el-col>
+			</el-form-item>
+			<el-form-item>
+				<el-col :span="10" :offset="6">
+					<el-button class="present" type="primary" @click="onSubmit">提&nbsp;&nbsp;&nbsp;&nbsp;交</el-button>
+				</el-col>
+			</el-form-item>
+  		</el-form>
+	</div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {}
+  export default {
+    data() {
+      return {
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        }
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!');
+      }
+    }
+  }
 </script>
 
 <style lang="less" rel="stylesheet/less">
+	.mt75{
+		margin-top: 75px;
+	}
+	.text-right{
+		text-align: right;
+	}
+	.right-result .right-title .iconfont{
+		margin-left: 15px;
+		margin-right: 12px;
+		font-size: 17px;
+		vertical-align: middle;
+	}
+	.right-result{
+		padding: 0px 3px;
+	}
+	.right-title{
+		border-bottom: 1px solid #c3d2d5;
+		height: 40px;
+		line-height: 40px;
+		font-weight: bold;
+	}
+	.getcode{
+		position: relative;
+	}
+	.testgetcode{
+		position: absolute;
+		right: 2px;
+		top: 2px;
+		padding: 8px;
+		background: #e2f5fc;
+		color: #2facdc;
+		border-color: #e2f5fc
+	}
+	.present{
+		background: #58bde4;
+		border-color: #58bde4;
+	}
 </style>
